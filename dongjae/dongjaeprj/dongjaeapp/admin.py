c657ee1e138c1ae56779f56028b0aae962bfc5bc
djdongjae <isf1999@naver.com>
2023-05-25 10:12:47 +0900

    제출합니다

---
+++ dongjae/dongjaeprj/dongjaeapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Ex_Post
+from .models import Ex2_Post
+
+# Register your models here.
+admin.site.register(Ex_Post)
+admin.site.register(Ex2_Post)
