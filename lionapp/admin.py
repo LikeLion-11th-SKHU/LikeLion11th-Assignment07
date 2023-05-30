86e861f21a421c14d1caa8d96d1aeb9cdb184bec
gosilver20012001 <fbigood365@gmail.com>
2023-05-30 20:51:05 +0900

    류고은 05.30 7주차 과제 제출03

--- lionapp/admin.py
+++ lionapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Lion_Post
+from .models import Lion_Post02
 
 # Register your models here.
 admin.site.register(Lion_Post)
+admin.site.register(Lion_Post02)
--- lionapp/views.py
+++ lionapp/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-from .models import Lion_Post
+from .models import Lion_Post,Lion_Post02
 # Create your views here.
-
-def main(request) :
-    return render(request,'main.html')
-
-def goeun(request) :
-    return render(request,'goeun.html')
-
 def main(request) :
     lion_posts = Lion_Post.objects.all
     return render(request,'main.html',{'lion_posts':lion_posts})
 
 def goeun(request) :
-    return render(request,'goeun.html',{'lion_post':Lion_Post.objects.all})
+    lion_post = Lion_Post02.objects.all
+    return render(request,'goeun.html',{'lion_post':lion_post})
